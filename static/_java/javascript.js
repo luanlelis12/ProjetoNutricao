3492c8a6399bb5a41e3c57b9ca59248b2396390a
luanlelis12 <luanlelis09@gmail.com>
2019-11-05 18:26:46 -0300

    JS, Imagens e css

--- static/_java/javascript.js
+++ static/_java/javascript.js
@@ -1,15 +1,3 @@
-function formatar(mascara, documento) {
-
-    var i = documento.value.length;
-    var saida = mascara.substring(0,1);
-    var texto = mascara.substring(i);
-
-    if (texto.substring(0,1) != saida) {
-        documento.value += texto.substring(0,1);
-    }
-
-}
-
 window.onscroll = function(){
 
     if (window.scrollY >= "350") {
