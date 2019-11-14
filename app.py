637c40550228aad79bb74a03a12483723ba49234
luanlelis12 <luanlelis09@gmail.com>
2019-11-14 16:12:45 -0300

    Rota dos sites

--- app.py
+++ app.py
@@ -1,41 +1,75 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+from formulario import CadastroForm, LoginForm
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BancoDeDados.db'
+app.config['SECRET_KEY'] = 'akjdshsagdgajdgmnxmch'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    nome = db.Column(db.String(60), nullable=False)
+    senha = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    sexo = db.Column(db.String(1), nullable=False)
+    aniversario = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '%r' % self.nome
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['POST', 'GET'])
 def cadastro():
-    return render_template('cadastro.html')
+
+    form = CadastroForm()
+
+    if form.validate_on_submit():
+        
+        user = User()
+        
+        user.nome = form.nome.data
+        user.email = form.email.data
+        user.sexo = form.sexo.data
+        user.aniversario = form.aniversario.data
+        user.senha = form.senha.data
+        
+        db.session.add(user)
+        db.session.commit()
+
+    return render_template('cadastro.html', form=form)
 
 @app.route('/dieta')
 def dieta():
     return render_template('dieta.html')
 
-@app.route('/doacao')
-def doacao():
-    return render_template('doacao.html')
-
-@app.route('/fala')
+@app.route('/fale conosco')
 def fala():
     return render_template('fala.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    
+    form = LoginForm()
+
+    if form.validate_on_submit():
+           
+        nome = form.nome.data
+        email = form.email.data
+        senha = form.senha.data
+        
+        senhadata = db.execute("SELECT senha FROM user WHERE senha=:senha",{"senha":senha}).fetchone()
+
+        if senha == senhadata:
+            return render_template('index.html') 
+
+            
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
