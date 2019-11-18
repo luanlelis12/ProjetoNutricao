e2bc9c76a2a489154dbf8ae0aaa552a04ce86285
luanlelis12 <luanlelis09@gmail.com>
2019-11-17 22:39:16 -0300

    Rotas dos sites

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from formulario import CadastroForm, LoginForm, FazerDieta
+from datetime import datetime
 
-from formulario import CadastroForm, LoginForm
+now = datetime.now()
+
+global user_logado
+user_logado = 'Fazer login'
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -9,6 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BancoDeDados.db'
 app.config['SECRET_KEY'] = 'akjdshsagdgajdgmnxmch'
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(60), nullable=False)
     senha = db.Column(db.String(20), unique=True, nullable=False)
@@ -18,6 +24,23 @@ class User(db.Model):
 
     def __repr__(self):
         return '%r' % self.nome
+
+class Dieta(db.Model):
+    __tablename__ = 'dieta'
+    codDieta = db.Column(db.Integer, primary_key=True)
+    peso = db.Column(db.Float, nullable=False)
+    altura = db.Column(db.Integer, nullable=False)
+    objetivo = db.Column(db.String(7), nullable=False)
+    TMB = db.Column(db.Float, nullable=False)
+    Proteinas = db.Column(db.Float, nullable=False)
+    Carboidratos =  db.Column(db.Float, nullable=False)
+    Gordura = db.Column(db.Float, nullable=False)
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+@app.context_processor
+def user_logado_context():
+    return dict(user_logado=user_logado)
+
 
 @app.route('/')
 @app.route('/index')
@@ -30,23 +53,39 @@ def cadastro():
     form = CadastroForm()
 
     if form.validate_on_submit():
-        
-        user = User()
-        
-        user.nome = form.nome.data
-        user.email = form.email.data
-        user.sexo = form.sexo.data
-        user.aniversario = form.aniversario.data
-        user.senha = form.senha.data
-        
-        db.session.add(user)
+        novo_user = User(nome = form.nome.data, email = form.email.data, sexo = form.sexo.data, aniversario = form.aniversario.data, senha = form.senha.data)
+        db.session.add(novo_user)
         db.session.commit()
+        return redirect(url_for('login'))
 
     return render_template('cadastro.html', form=form)
 
-@app.route('/dieta')
+@app.route('/dieta', methods=['POST', 'GET'])
 def dieta():
-    return render_template('dieta.html')
+
+    form = FazerDieta()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(nome=user_logado).first()
+        peso = form.peso.data
+        altura = form.altura.data
+        dNiver = user.aniversario
+        yNiver = dNiver.year
+        yNow = now.year
+        idade = yNow-yNiver
+        print(idade)
+        Proteinas = 2.5*peso
+        Gordura = 1*peso
+        Carboidratos = 0
+        if user.sexo is 'F':
+            TMB = (655+(9.6*peso)+(1.8*altura)-(4.7*idade))
+        if user.sexo is 'M':
+            TMB = (66+(13.8*peso)+(5*altura)-(6.8*idade))
+        nova_dieta = Dieta(peso = form.peso.data, altura = form.altura.data, objetivo = form.objetivo.data, TMB = TMB, Proteinas = Proteinas, Carboidratos = Carboidratos, Gordura = Gordura, id = user.id)
+        db.session.add(nova_dieta)
+        db.session.commit()
+
+    return render_template('dieta.html', form=form)
 
 @app.route('/fale conosco')
 def fala():
@@ -59,16 +98,16 @@ def login():
 
     if form.validate_on_submit():
            
-        nome = form.nome.data
-        email = form.email.data
-        senha = form.senha.data
-        
-        senhadata = db.execute("SELECT senha FROM user WHERE senha=:senha",{"senha":senha}).fetchone()
+        user = User.query.filter_by(nome=form.nome.data).first()
 
-        if senha == senhadata:
-            return render_template('index.html') 
+        if user:
+            if user.senha == form.senha.data and user.email == form.email.data:
+                global user_logado
+                user_logado = form.nome.data
+                return redirect(url_for('index'))
+            else:
+                return 'osdjajdksa'
 
-            
     return render_template('login.html', form=form)
 
 if __name__ == '__main__':
