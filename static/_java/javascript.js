231fa5dac61c40e279886da2c5579686bdf23411
luanlelis12 <luanlelis09@gmail.com>
2019-12-02 20:49:58 -0300

    CS, JS e imagens

--- static/_java/javascript.js
+++ static/_java/javascript.js
@@ -1,12 +1,20 @@
 window.onscroll = function(){
 
-    if (window.scrollY >= "250") {
+    if (window.scrollY >= "200") {
         document.getElementById("conteudo_article").style.opacity = 1;
         document.getElementById("conteudo_article").style.left = 0;
     }
     else{
         document.getElementById("conteudo_article").style.opacity = 0;
         document.getElementById("conteudo_article").style.left = "5%";
+    }
+}
+
+window.onload = function() {
+    var link = document.getElementById('link').innerHTML;
+    if (link != 'Fazer login'){
+        document.getElementById("link").href="/dieta";
+        document.getElementById("titulo_site").innerHTML="<div id='slogan'><span id='slogan_site'>Uma simples<br/>dieta para sua<br/>saúde.</span><hr id='hr_slogan'/></div>";
     }
 }
 
